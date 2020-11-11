e507e3fda1344087f1f40d51382e6065c8317761
domzgod <73922449+domzgod@users.noreply.github.com>
2020-11-11 15:21:47 +0800

    Add files via upload

---
+++ oddeven.c
@@ -0,0 +1,12 @@
+int main()
+{
+    int domz;
+    printf("Enter a number:");
+    scanf("%d",&domz);
+
+    if(domz % 2 == 0)
+        printf("%d is even number.", domz);
+    else
+        printf ("%d is an odd number.", domz);
+    return 0;
+}
