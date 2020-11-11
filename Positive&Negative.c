5ee8e608e3045138c8ec8fd332e60c0da4658263
domzgod <73922449+domzgod@users.noreply.github.com>
2020-11-11 15:39:41 +0800

    Add files via upload

---
+++ Positive&Negative.c
@@ -0,0 +1,12 @@
+int main()
+{
+    int domz;
+        printf("Enter a number:");
+        scanf("%d",&domz);
+
+        if (domz >= 0)
+            printf("%d is a positive number.", domz);
+        else
+            printf("%d is a negative number.", domz);
+        return 0;
+}
